b6ee14fc941931870f05ec65a0c1ccef90882de8
João Afonso <joaoafonsoassumpcao@gmail.com>
2022-06-21 10:42:05 -0300

    web app

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,9 @@
-console.log("hi there")
+import { User } from "./models/User";
+
+const user = new User({name: "MyName", age: 20})
+
+user.on("change", () => {
+    console.log("changing #1")
+})
+
+user.trigger("change")
